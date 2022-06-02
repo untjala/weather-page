fc6ae9c928d75e009160598b0f57408dab60085a
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-02 10:08:56 -0400

    pulled temp, wind speed, uv, and humidity from openweather

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 //Global Variables
-var city = $('.searchCity').val();
+var searchCity = $('#searchCity');
+var searchEl = $('#searchBtn')
+var currentCity = $('.currentCity')
 // Api Fetch
 fetch('http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=50776dd8bb98783725e832a860968c49')
     .then(function(response){
@@ -16,6 +18,20 @@ function getWeatherData(lat,lon) {
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+        console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
+        var temp = data.current.temp;
+        var wind_speed = data.current.wind_speed
+        var uvi = data.current.uvi;
+        var humidity = data.current.humidity
+        var tempEl = temp(document.createElement('p'));
+        var windEl = document.createElement('p');
+        var uviEl = document.createElement('p');
+        var humidityEl = document.createElement('p');
+        document.body.append(tempEl, windEl, uviEl, humidityEl)
     });
 }
+//Event listeners
+// searchEl.addEventListner('click', function(){
+//     var input = searchCity.value;
+//     getWeatherData(input);
+// })
