dddc0445404705453cea44b93dacd38fbbda0ea0
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-02 19:44:56 -0400

    tweaks to fetch, weather vars apppended

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,23 @@
+// // $("#currentDay").text(moment().format("dddd, MMMM Do"));
+// //Global Variables
+// var forecastContainer = document.getElementById('forecast')
+// var searchCityEl = document.getElementById('searchCity');
+// var searchEl = document.getElementById('searchBtn')
+// var currentCityEl = document.getElementById('currentCity')
+// var apiKey = '50776dd8bb98783725e832a860968c49'
+
+// //Erases browser defaults for form
+// var inputForm = function (event) {
+//     event.preventDefault();
+// }
+
+// //Search button click event 
+// searchEl.click(function (event) {
+//     console.log(searchCityEl.value)
+//     var city = searchCityEl.value
+//     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=50776dd8bb98783725e832a860968c49`)
+//         .then(function (response) {
+//             return response.json();
 //Global Variables
 var currentTimeEl = document.getElementById('#currentDay');
 var searchBtnEl = document.getElementById('#searchBtn');
@@ -9,7 +29,7 @@ var fiveForecastEl = document.getElementById('#fiveDayForecast');
 var apiKey = '50776dd8bb98783725e832a860968c49'
 
 //Erases browser defaults for form
-var inputForm = function (event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     //Runs function to display data if city is entered, display error message otherwise
     var searchedCity = inputNameEl.value;
@@ -23,18 +43,26 @@ var inputForm = function (event) {
 };
 
 //Search button click event 
-searchBtnEl.addEventListener('click', function (event) {
-    console.log(searchedCityEl.value)
-    // Api Fetch for geo location
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&limit=1&appid=50776dd8bb98783725e832a860968c49`)
-    .then(function (response) {
-        return response.json();
+var buttonClickHandller = function (event) {
+    searchBtnEl.addEventListener('click', function (event) {
+        console.log(searchedCityEl.value)
     })
-    .then(function (data) {
-        console.log(data[0].lat, data[0].lon);
-        getWeatherData(data[0].lat, data[0].lon);
-    });
-})
+}
+// Api Fetch for geo location
+var getLocation = function (user) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&limit=1&appid=50776dd8bb98783725e832a860968c49`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data[0].lat, data[0].lon);
+            getWeatherData(data[0].lat, data[0].lon);
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+};
+
 
 //OpenWeather API data intergration
 var displayWeather = function (response) {
@@ -42,57 +70,130 @@ var displayWeather = function (response) {
     var cityNameEl = document.createElement('h1');
     cityNameEl.setAttribute('class', 'cityNameDisplay');
     selectedCityEl.append(cityNameEl)
-//Appends tempature to current city weather container
+    //Appends tempature data to current city weather container
+    var temp = response.main.temp;
+    tempEl.setAttribute('class', 'tempDisplay')
     var tempEl = document.createElement('div');
-    tempEl.textContent = 'It is: ' + 'degrees outside.'
-//Appends Humidity to current city weather container 
+    tempEl.textContent = 'It is: ' + temp + 'degrees outside.'
+    selectedCityEl.append(tempEl)
+    //Appends humidity data to current city weather container 
+    var humidity = response.main.humidity;
+    humidityEl.setAttribute('class', 'humidityDisplay');
+    var humidityEl = document.createElement('div');
+    humidityEl.textContent = 'Humidity' + humidity + '%'
+    selectedCityEl.append(humidityEl)
+
+    //Appends wind speed data to current city weather container
+    var windSpeed = response.wind.speed;
+    windSpeedEl.setAttribute('class', 'windDisplay');
+    var windSpeedEl = document.createElement('div');
+    humidityEl.textContent = 'Wind Speed: ' + windSpeed + 'MPH'
+    selectedCityEl.append(windEl)
+    //Appends UV index to current city weather container 
+    var uvi = response.main.uvi;
+    uviEl.setAttribute('class', 'uviDisplay');
+    var uviEl = document.createElement('div');
+    uviEl.textContent = 'UV Index' + uvi
+    selectedCityEl.append(uviEl)
 }
 
 //Api fetch for temp, wind speed, uv index, and humidity.
-function getWeatherData(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=50776dd8bb98783725e832a860968c49`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
+// function getWeatherData(lat, lon) {
+    // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=50776dd8bb98783725e832a860968c49`)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
 
-            //Tutor Assistance, logs the daily data in seconds, instead of milliseconds
-            console.log(data.daily[0].dt);
-            var time = data.daily[0].dt;
-            var date = new Date(time * 1000)
-            //End of Tutor Assistance
+//             //Tutor Assistance, logs the daily data in seconds, instead of milliseconds
+//             console.log(data.daily[0].dt);
+//             var time = data.daily[0].dt;
+//             var date = new Date(time * 1000)
+//             //End of Tutor Assistance
 
-            console.log(date);
-            for (var i = 0; i < 5; i++) {
-                var day = data.daily[i]
-                console.log(day)
-                var dailyCard = document.createElement('div');
-                dailyCard.setAttribute('class', 'five-card')
-                forecastContainer.append(dailyCard)
+//             console.log(date);
+//             for (var i = 0; i < 5; i++) {
+//                 var day = data.daily[i]
+//                 console.log(day)
+//                 var dailyCard = document.createElement('div');
+//                 dailyCard.setAttribute('class', 'five-card')
+//                 forecastContainer.append(dailyCard)
 
-                //Set and pull temp variables
-                var temp = data.current.temp;
-                var wind = data.current.wind_speed
-                var uvi = data.current.uvi;
-                var humidity = data.current.humidity
+//                 //Set and pull temp variables
+//                 var temp = data.current.temp;
+//                 var wind = data.current.wind_speed
+//                 var uvi = data.current.uvi;
+//                 var humidity = data.current.humidity
 
-                //Create Elements from variables 
-                var tempEl = document.createElement('p');
-                var windEl = document.createElement('p');
-                var uviEl = document.createElement('p');
-                var humidityEl = document.createElement('p');
+//                 //Create Elements from variables 
+//                 var tempEl = document.createElement('p');
+//                 var windEl = document.createElement('p');
+//                 var uviEl = document.createElement('p');
+//                 var humidityEl = document.createElement('p');
 
-                //Displays weather content 
-                temp.textContent = "Temp: " + temp;
-                tempEl.textContent = "Temp: " + temp;
-                windEl.textContent = "Wind: " + wind;
-                uviEl.textContent = "UV Index: " + uvi;
-                humidityEl.textContent = "Humidity: " + humidity;
-                // document.body.append(tempEl, windEl, uviEl, humidityEl)
-            }
-        });
-}
+//                 //Displays weather content 
+//                 temp.textContent = "Temp: " + temp;
+//                 tempEl.textContent = "Temp: " + temp;
+//                 windEl.textContent = "Wind: " + wind;
+//                 uviEl.textContent = "UV Index: " + uvi;
+//                 humidityEl.textContent = "Humidity: " + humidity;
+//                 // document.body.append(tempEl, windEl, uviEl, humidityEl)
+//             }
+//         });
+
 //Store searches to local history/recall when clicked
 //Event Listners 
-searchBtnEl.onClick = inputForm;
+// searchBtnEl.onClick = formSubmitHandler;
+    // .then(function (data) {
+    //     console.log(data[0].lat, data[0].lon);
+    //     getWeatherData(data[0].lat, data[0].lon);
+    // });
+
+
+    //Api fetch for temp, wind speed, uv index, and humidity.
+    // Api Fetch for geo location
+    // function getWeatherData(lat, lon) {
+    //     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=50776dd8bb98783725e832a860968c49`)
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (data) {
+    //             console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
+
+    //             //Tutor Assistance, logs the daily data in seconds, instead of milliseconds
+    //             console.log(data.daily[0].dt);
+    //             var time = data.daily[0].dt;
+    //             var date = new Date(time * 1000)
+    //             //End of Tutor Assistance
+
+    //             console.log(date);
+    //             for (var i = 0; i < 5; i++) {
+    //                 var day = data.daily[i]
+    //                 console.log(day)
+    //                 var dailyCard = document.createElement('div');
+    //                 dailyCard.setAttribute('class', 'five-card')
+    //                 forecastContainer.append(dailyCard)
+
+    //                 //Set and pull temp variables
+    //                 var temp = data.current.temp;
+    //                 var wind = data.current.wind_speed
+    //                 var uvi = data.current.uvi;
+    //                 var humidity = data.current.humidity
+
+    //                 //Create Elements from variables
+    //                 var tempEl = document.createElement('p');
+    //                 var windEl = document.createElement('p');
+    //                 var uviEl = document.createElement('p');
+    //                 var humidityEl = document.createElement('p');
+
+    //                 //Displays weather content
+    //                 temp.textContent = "Temp: " + temp;
+    //                 tempEl.textContent = "Temp: " + temp;
+    //                 windEl.textContent = "Wind: " + wind;
+    //                 uviEl.textContent = "UV Index: " + uvi;
+    //                 humidityEl.textContent = "Humidity: " + humidity;
+    //                 // document.body.append(tempEl, windEl, uviEl, humidityEl)
+    //             }
+    //         });
+    // }
