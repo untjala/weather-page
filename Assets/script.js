c05236cad68ea8f91aab3c73cb1c63da76fea239
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-02 16:20:07 -0400

    set date display, adjusted for loop to display four results

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
+// $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //Global Variables
-var searchCity = $('#searchCity');
+
+var searchCityEl = document.getElementById('searchCity');
 var searchEl = $('#searchBtn')
-var currentCity = $('.currentCity')
+var currentCityEl = $('.currentCity')
 var apiKey = '50776dd8bb98783725e832a860968c49'
 
 //Erases browser defaults for form
@@ -10,8 +12,9 @@ var inputForm = function (event) {
 }
 //Search button click event 
 searchEl.click(function (event) {
-    console.log("hello")
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=50776dd8bb98783725e832a860968c49')
+    console.log(searchCityEl.value)
+    var city = searchCityEl.value
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=50776dd8bb98783725e832a860968c49`)
         .then(function (response) {
             return response.json();
         })
@@ -25,17 +28,21 @@ searchEl.click(function (event) {
 
 //Api fetch for temp, wind speed, uv index, and humidity.
 function getWeatherData(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=50776dd8bb98783725e832a860968c49`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=50776dd8bb98783725e832a860968c49`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
-
+            //Tutor Assistance
+            console.log(data.daily[0].dt);
+            var time = data.daily[0].dt;
+            var date = new Date(time * 1000)
+            //End of Tutor Assistance
+            console.log(date);
             for (var i = 0; i < 5; i++) {
                 var day = data.daily[i]
                 console.log(day)
-            }
             var temp = data.current.temp;
             var wind = data.current.wind_speed
             var uvi = data.current.uvi;
@@ -51,6 +58,6 @@ function getWeatherData(lat, lon) {
             uviEl.textContent = "UV Index: " + uvi;
             humidityEl.textContent = "Humidity: " + humidity;
             document.body.append(tempEl, windEl, uviEl, humidityEl)
+            }
         });
 }
-//Event listners 
