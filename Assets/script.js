43a82286a5c7afc8ddcda3c305cf0f46d257908b
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-02 09:30:48 -0400

    added api fetch requests

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,21 @@
 //Global Variables
-var city =
+var city = $('.searchCity').val();
+// Api Fetch
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=50776dd8bb98783725e832a860968c49')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data[0].lat, data[0].lon);
+        getWeatherData(data[0].lat, data[0].lon);
+    });
+
+function getWeatherData(lat,lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=50776dd8bb98783725e832a860968c49`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    });
+}
