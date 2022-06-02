804ff9624e7b226ce51b8796c99acfa5f3c7ed7d
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-02 14:53:18 -0400

    pulled and appended current weather data from api to document body

--- Assets/script.js
+++ Assets/script.js
@@ -2,36 +2,43 @@
 var searchCity = $('#searchCity');
 var searchEl = $('#searchBtn')
 var currentCity = $('.currentCity')
-// Api Fetch
+// Api Fetch for geo location
 fetch('http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=50776dd8bb98783725e832a860968c49')
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data[0].lat, data[0].lon);
         getWeatherData(data[0].lat, data[0].lon);
     });
 
-function getWeatherData(lat,lon) {
+//Api fetch for temp, wind speed, uv index, and humidity.
+function getWeatherData(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=50776dd8bb98783725e832a860968c49`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
-        var temp = data.current.temp;
-        var wind_speed = data.current.wind_speed
-        var uvi = data.current.uvi;
-        var humidity = data.current.humidity
-        var tempEl = temp(document.createElement('p'));
-        var windEl = document.createElement('p');
-        var uviEl = document.createElement('p');
-        var humidityEl = document.createElement('p');
-        document.body.append(tempEl, windEl, uviEl, humidityEl)
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.current.temp, data.current.wind_speed, data.current.uvi, data.current.humidity,);
+
+            for (var i= 0; i < 5; i++) {
+                var day = data.daily[i]
+                console.log(day)
+            }
+            var temp = data.current.temp;
+            var wind = data.current.wind_speed
+            var uvi = data.current.uvi;
+            var humidity = data.current.humidity
+            var tempEl = document.createElement('p');
+            var windEl = document.createElement('p');
+            var uviEl = document.createElement('p');
+            var humidityEl = document.createElement('p');
+
+            temp.textContent = "Temp: " + temp;
+            tempEl.textContent = "Temp: " + temp;
+            windEl.textContent = "Wind: " + wind;
+            uviEl.textContent = "UV Index: " + uvi;
+            humidityEl.textContent = "Humidity: " + humidity;
+            document.body.append(tempEl, windEl, uviEl, humidityEl)
+        });
 }
-//Event listeners
-// searchEl.addEventListner('click', function(){
-//     var input = searchCity.value;
-//     getWeatherData(input);
-// })
